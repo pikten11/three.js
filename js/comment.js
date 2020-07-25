f9c84d99bedd3bebaa211860d9213b96c5ddcfbe
pikten11 <66857832+pikten11@users.noreply.github.com>
2020-07-25 11:23:33 +0900

    Update comment.js

--- js/comment.js
+++ js/comment.js
@@ -7,5 +7,7 @@ function comment(){
   html += '<a href="material_texture.html">3.球体-地球-</a>';
   html += '<p><br></p>';
   html += '<a href="geometry.html">4.様々な形</a>';
+  html += '<p><br></p>';
+  html += '<a href="earth.html">5.地球を中心としてカメラを回転させる</a>';
   document.write(html);
 }
