8e219d7ce434f7c413d8760e1b424bd7ac0e74a0
pikten11 <66857832+pikten11@users.noreply.github.com>
2020-07-25 11:01:25 +0900

    Update comment.js

--- js/comment.js
+++ js/comment.js
@@ -3,5 +3,7 @@ function comment(){
   html += '<a href="index.html">1.箱が回転<a>';
   html += '<br>';
   html += '<a href="material.html">2.球体</a>';
+  html += '<br>';
+  html += '<a href="material_texture.html">3球体-地球-</a>';
   document.write(html);
 }
