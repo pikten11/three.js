78184e80f6032ff6a8ceac93d91f5ee18448b8c8
pikten11 <66857832+pikten11@users.noreply.github.com>
2020-07-25 11:26:46 +0900

    Update comment.js

--- js/comment.js
+++ js/comment.js
@@ -9,5 +9,7 @@ function comment(){
   html += '<a href="geometry.html">4.様々な形</a>';
   html += '<p><br></p>';
   html += '<a href="earth.html">5.地球を中心としてカメラを回転させる</a>';
+  html += '<p><br></p>';
+  html += '<a href="earth_rotation">6.マウスの座標に応じて回転させる</a>';
   document.write(html);
 }
