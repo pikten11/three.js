ee1706cac22423bc005930364b50e3c4f79b7643
pikten11 <66857832+pikten11@users.noreply.github.com>
2020-07-25 10:55:32 +0900

    Update comment.js

--- js/comment.js
+++ js/comment.js
@@ -1,2 +1,6 @@
-function header(){
-var html = "";
+function comment(){
+  var html = "";
+  html += '<a href="index.html">1.箱が回転<a>';
+  html += '<a href="material.html">2.球体</a>';
+  document.write(html);
+}
